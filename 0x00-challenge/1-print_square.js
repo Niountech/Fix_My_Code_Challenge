26dde6f8a8682406f4611891ef38e5ae5a5ee8c0
Proseken <niountech@gmail.com>
2024-05-28 12:47:27 +0100

    edited

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -5,12 +5,11 @@
     of the program.
 */
 
-
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+    process.exit(1);  // Added missing semicolon here
 }
 
 const size = parseInt(process.argv[2], 10);
@@ -21,8 +20,9 @@ if (isNaN(size) || size <= 0) {
     process.stderr.write("Example: ./1-print_square.js 8\n");
     process.exit(1);
 }
-for (let i = 0; i < size ; i++) {
-    for (let j = 0; j < size ; j++) {
+
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
